e852dd5fb48c38556ea6bdf3b428a981560eccf2
Chris Wiederspan <chwieder@microsoft.com>
2018-08-27 15:30:31 -0600

    Added command line parameter support to the ACI file

--- deploy-to-aci.sh
+++ deploy-to-aci.sh
@@ -4,12 +4,21 @@
 echo "Starting job..."
 
 # Set the variables for creating the resources
-NAME="my-drupal-container"
-LOCATION="WESTUS2"
+name="my-drupal-container"
+location="westus2"
+
+# Override any of the hard-coded variable names with command line params
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        value=$(echo "${1/--/}"|sed "s/-/_/")
+        declare $value="$2"
+    fi
+    shift
+done
 
 # Create the Resource Group
-echo "Creating ACI container $NAME in $LOCATION..."
-az group create --name $NAME --location $LOCATION
+echo "Creating ACI container $name in $location..."
+az group create --name $name --location $location
 
 # Create the ACI container
-az container create --name $NAME --resource-group $NAME --location $LOCATION --image drupal:latest --ports 80 443 --ip-address Public --dns-name-label $NAME
+az container create --name $name --resource-group $name --location $location --image drupal:latest --ports 80 443 --ip-address Public --dns-name-label $name
